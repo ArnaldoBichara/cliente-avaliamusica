f6829c1cf4b1d40282356f68d441cc391d7d7b0c
ARNALDO ALMEIDA <arnaldo.bichara@gmail.com>
2021-09-25 11:26:18 -0300

    botões gerando ações

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,113 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const AvaliaMusica());
+  runApp(const MyApp());
 }
 
-class AvaliaMusica extends StatelessWidget {
-  const AvaliaMusica({Key? key}) : super(key: key);
+Column _buildButtonColumn(String texto, Color cor, bool botaoEstaSelecionado,
+    IconData iconeSelec, IconData iconeNaoSelec, var funcaoAChamar) {
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      IconButton(
+        icon: (botaoEstaSelecionado ? Icon(iconeSelec) : Icon(iconeNaoSelec)),
+        color: cor,
+        onPressed: funcaoAChamar,
+      ),
+      Text(
+        texto,
+        style: TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.w400,
+          color: cor,
+        ),
+      ),
+      //),
+    ],
+  );
+}
 
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Avalia Música',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: const MyHomePage(title: 'Avalia Música'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _boolCurteAMusica = false;
+  void _botaoCurteAcionado() {
+    setState(() {
+      if (_boolCurteAMusica) {
+        _boolCurteAMusica = false;
+      } else {
+        _boolCurteAMusica = true;
+      }
+    });
+  }
+
+  bool _boolNaoCurteAMusica = false;
+  void _botaoNaoCurteAcionado() {
+    setState(() {
+      if (_boolNaoCurteAMusica) {
+        _boolNaoCurteAMusica = false;
+      } else {
+        _boolNaoCurteAMusica = true;
+      }
+    });
+  }
+
+  bool _boolIndiferenteAMusica = false;
+  void _botaoIndiferenteAcionado() {
+    setState(() {
+      if (_boolIndiferenteAMusica) {
+        _boolIndiferenteAMusica = false;
+      } else {
+        _boolIndiferenteAMusica = true;
+      }
+    });
+  }
+
+  bool _boolAvaliarMus = false;
+  void _botaoAvaliarMusAcionado() {
+    setState(() {
+      if (_boolAvaliarMus) {
+        _boolAvaliarMus = false;
+      } else {
+        _boolAvaliarMus = true;
+      }
+    });
+  }
+
+  bool _boolGetStats = false;
+  void _botaoEstatsAcionado() {
+    setState(() {
+      if (_boolGetStats) {
+        _boolGetStats = false;
+      } else {
+        _boolGetStats = true;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     Color color = Theme.of(context).primaryColor;
-
     Widget regiaoDoTitulo = Container(
       padding: const EdgeInsets.all(12),
       child: Column(
@@ -29,7 +125,6 @@ class AvaliaMusica extends StatelessWidget {
         ],
       ),
     );
-
     Widget regiaoMsgExplicativa = const Padding(
       padding: EdgeInsets.all(24),
       child: Text(
@@ -39,21 +134,22 @@ class AvaliaMusica extends StatelessWidget {
         softWrap: true,
       ),
     );
-
     Widget regiaoDosBotoesDeEscolha = Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        _buildButtonColumn(Colors.greenAccent, Icons.favorite, 'Gosto'),
-        _buildButtonColumn(
-            Colors.redAccent, Icons.favorite_border, 'Não Gosto'),
-        _buildButtonColumn(color, Icons.flutter_dash, 'Indiferente'),
+        _buildButtonColumn('Curto!', Colors.greenAccent, _boolCurteAMusica,
+            Icons.favorite, Icons.favorite_border, _botaoCurteAcionado),
+        _buildButtonColumn('Não Curto', Colors.redAccent, _boolNaoCurteAMusica,
+            Icons.favorite, Icons.favorite_border, _botaoNaoCurteAcionado),
+        _buildButtonColumn('Indiferente', Colors.brown, _boolIndiferenteAMusica,
+            Icons.favorite, Icons.favorite_border, _botaoIndiferenteAcionado),
       ],
     );
     Widget regiaoDosBotoesDeAcao = Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        _buildButtonColumn(color, Icons.add, 'Nova Avaliação'),
-        _buildButtonColumn(color, Icons.query_stats, 'Estatísticas'),
+        _buildButtonColumn1(color, Icons.add, 'Nova Avaliação'),
+        _buildButtonColumn1(color, Icons.query_stats, 'Estatísticas'),
       ],
     );
     Widget regiaoNomeDaMusica = Container(
@@ -83,7 +179,6 @@ class AvaliaMusica extends StatelessWidget {
         ],
       ),
     );
-
     Widget regiaoMsgResultado = Container(
       padding: const EdgeInsets.all(24),
       child: Row(
@@ -137,7 +232,7 @@ class AvaliaMusica extends StatelessWidget {
   }
 }
 
-Column _buildButtonColumn(Color color, IconData icon, String label) {
+Column _buildButtonColumn1(Color color, IconData icon, String label) {
   return Column(
     mainAxisSize: MainAxisSize.min,
     mainAxisAlignment: MainAxisAlignment.center,
@@ -156,94 +251,4 @@ Column _buildButtonColumn(Color color, IconData icon, String label) {
       ),
     ],
   );
-}
-
-class MinhaHomePage extends StatefulWidget {
-  const MinhaHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MinhaHomePage> createState() => _MinhaHomePageState();
-}
-
-class _MinhaHomePageState extends State<MinhaHomePage> {
-  int _counter = 0;
-  String linha1 = "minha primeira linha:";
-  String linha2 = "minha segunda linha:";
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MinhaHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              linha1,
-            ),
-            Text(
-              linha2,
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
 }
